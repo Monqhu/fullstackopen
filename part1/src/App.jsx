8f360de6ebc2f6977a63fb816307b7e3ae067337
Adria <adriamoncu@gmail.com>
2024-12-12 15:21:31 +0100

    acabados los ejercicios de teoría, empiezan los ejercicios de práctica

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,7 +1,32 @@
-const App = () => {
+const Hello = () => {
+  const name = 'Peter'
+  const age = 10
+
+  return(
+    <>
+      <div>
+        <p>Hello {name}, you are {age} years old</p>
+      </div>
+    </>
+  )
+}
+
+const Footer = () => {
   return (
     <div>
-      <p>Hello world</p>
+      greeting app created by <a href='https://github.com/mluukkai'>mluukkai</a>
+    </div>
+  )
+}
+
+
+const App = () => {
+  const friends = [ 'Peter', 'Maya']
+
+  return (
+    
+    <div>
+      <p>{friends}</p>
     </div>
   )
 }
