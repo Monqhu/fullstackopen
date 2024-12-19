419e7a2c3513043df590579d8f749d1b8990928a
Adria <adriamoncu@gmail.com>
2024-12-19 12:40:34 +0100

    ejercicio 1.7 completado y debuggeado

--- part1/informacionDelCurso/src/App.jsx
+++ part1/informacionDelCurso/src/App.jsx
@@ -1,34 +1,130 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const Button = (props) => {
+  return(
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
+}
+
+const Display = (props) => {
+  return(
+    <div>
+      {props.text}: {props.total}
+    </div>
+  )
+}
+
+const App = () => {
+  // guarda los clics de cada botón en su propio estado
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(good + neutral + bad)
+  const [average, setAverage] = useState('Waiting for data')
+  const [positive, setPositive] = useState('Doing some high-tech math stuff...')
+
+
+  /*
+  const operation = (type) => {
+
+    
+    console.log(type)
+
+    if (type === undefined) return null;
+
+    if (type === "sum") {
+      
+      setGood((prev) => prev + 1);
+
+    }
+    if (type === "rest") {
+      setBad((prev) => prev - 1);
+    }
+    if (type === "mult") {
+      setNumero((prev) => prev * 2);
+    }
+
+
+  };
+  */
+
+
+  const operate = (operate) => {
+    let goodUpdated = good
+    let badUpdated = bad
+    let neutralUpdated = neutral
+    let allUpdated = all
+
+    
+    switch (operate) {
+      case 'good':
+
+        setGood((prev) => prev + 1)
+        goodUpdated = goodUpdated + 1
+        allUpdated = allUpdated + 1
+        setAll(goodUpdated + neutral +bad)
+        calculateAverage(goodUpdated, neutralUpdated, badUpdated, allUpdated)
+
+        calculatePositive(allUpdated, goodUpdated)
+      break;
+    
+      case 'neutral': 
+        setNeutral((prev) => prev +1)
+        neutralUpdated = neutralUpdated + 1
+        allUpdated = allUpdated + 1
+        setAll(good + neutralUpdated + bad)   
+        calculateAverage(goodUpdated, neutralUpdated, badUpdated, allUpdated)
+        calculatePositive(allUpdated, goodUpdated)
+
+      break;
+
+      case 'bad':
+        setBad((prev) => prev + 1)
+        badUpdated = badUpdated + 1
+        allUpdated = allUpdated + 1
+        setAll(good + neutral + badUpdated)
+        calculateAverage(goodUpdated, neutralUpdated, badUpdated, allUpdated)
+      
+        calculatePositive(allUpdated, goodUpdated)
+
+      break;
+    }
+  }
+
+  const calculateAverage = (goodUpdated, neutralUpdated, badUpdated, allUpdated) => {
+    let averageCalculated = (goodUpdated - badUpdated) / allUpdated
+    setAverage(averageCalculated)
+  }
+
+  const calculatePositive = (allUpdated, goodUpdated) => {
+    let positiveUpdated = goodUpdated * 100 / allUpdated
+    setPositive(positiveUpdated)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>give feedback</h1>
+      <Button onClick={() => operate('good')} text="good" />
+      <Button onClick={() => operate('neutral')} text="neutral" />
+      <Button onClick={() => operate('bad')} text="bad" />
+
+      <Button onClick={() => operation('sum')} text="Frank:Sum" />
+
+
+    
+      <h1>Statistics</h1>
+      <Display text="good" total={good} />
+      <Display text="neutral" total={neutral} />
+      <Display text="bad" total={bad} />
+      <Display text="all" total={all} />
+      <Display text="average" total={average} />
+      <Display text="Positive comments (in %)" total={positive} />
+    
+    
+    </div>
   )
 }
 
--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,34 +1,32 @@
-const Hello = () => {
-  const name = 'Peter'
+
+const Hello = (props) => {
+ const  {name, age, color} = props
+  console.log(props)
+  return (
+    <div>
+      <p>
+        Hello {name}, you are {age} years old {color}
+      </p>
+      <p style={{backgroundColor: color}}>
+        AMPARO
+      </p>
+    </div>
+  )
+}
+
+
+
+function App ()  {
+  const pepe = 'Peter'
   const age = 10
 
-  return(
-    <>
-      <div>
-        <p>Hello {name}, you are {age} years old</p>
-      </div>
-    </>
-  )
-}
-
-const Footer = () => {
   return (
     <div>
-      greeting app created by <a href='https://github.com/mluukkai'>mluukkai</a>
+      <h1>Greetings</h1>
+      <Hello name='Maya' age={26 + 10} />
+      <Hello name={pepe} age={age} />
+      <Hello color='red' />
     </div>
   )
 }
-
-
-const App = () => {
-  const friends = [ 'Peter', 'Maya']
-
-  return (
-    
-    <div>
-      <p>{friends}</p>
-    </div>
-  )
-}
-
 export default App
